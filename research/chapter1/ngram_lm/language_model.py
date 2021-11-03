0fd8cd792afda29399f71d65b5a97a1ccbcfe5be
이진기(AI/Bigdata) <jinki.lee@poscoict.com>
2021-11-03 22:42:07 +0900

    update language_model.py

--- research/chapter1/ngram_lm/language_model.py
+++ research/chapter1/ngram_lm/language_model.py
@@ -1,18 +1,65 @@
+import nltk
 from collections import Counter
+from functools import reduce
 
-text = '''After four years of refusing to hold Donald Trump accountable for his lies, conspiracy theories and hateful rhetoric, Republicans passed up another chance to purge those forces from their ranks Thursday when they overwhelmingly opposed Democrats' efforts to rebuke Georgia Rep. Marjorie Taylor Greene. The GOP is complaining that Democratic leaders are not only overreaching but also setting a dangerous precedent in both punishing Greene and pursuing a doomed-to-fail impeachment trial for an ex-President in the Senate next week. But genuinely unprecedented events have forced Democrats to take action. Despite national outrage about Trump's undemocratic actions, only 10 House Republicans voted to impeach him last month. And most Republicans balked Thursday at punishing Greene for espousing the dangerous lies and violent rhetoric that threaten the future of their party, with only 11 House Republicans joining Democrats in voting to kick Greene off her committees. Before being elected to Congress from Georgia, Greene compiled a long list of unhinged comments and social media posts, including endorsement of violence against and assassinations of top Democrats, 9/11 trutherism and denials of school shootings. For weeks, she was unrepentant -- at least until her fate was sealed Thursday, when she showed shades of contrition on the House floor -- though her combative speech adopted many of the outlandish tropes of Trumpism.'''
+text = '''she sells sea-shells by the sea-shore. the shells she sells are sea-shells, I'm sure. for if she sells sea-shells by the sea-shore then i'm sure she sells sea-shore shells.'''
 
-def preprocess(text):
-    text = text.lower()
-    text = text.split(' ')
-    return text
+def preprocess(sentences, n):
+    '''Split list of sentences into words and make a list of words
+
+    Args:
+        sentences (list of str): a list of sentences
+        n (int): variable N for N-gram model. How many words you want to see before a target word.
+    Returns:
+        A list of tokens which were tokenized from each sentence
+    '''
+
+    BOS = '<s>'
+    EOS = '</s>'
+    UNK = '<unk>'
+
+    # STEP1: 소문자 치환하기
+    sentences = list(map(str.lower, sentences))
+
+    # STEP2: BOS, EOS 추가하기
+    BOSs = ' '.join([BOS]*(n-1) if n > 1 else [BOS])
+    sentences = [' '.join([BOSs, s, EOS]) for s in sentences]
+
+    # STEP3: 토큰화하기
+    sentences = list(map(lambda s: s.split(), sentences))
+    tokens = list(reduce(lambda a, b: a+b, sentences))
+
+    # STEP4: 한번 출현한 단어 UNK으로 치환하기
+    freq = nltk.FreqDist(tokens)
+    tokens = [t if freq[t] > 1 else UNK for t in tokens]
+
+    return tokens
+
 
 class SimpleNgramLanguageModel():
-    def __init__(self, train_data):
-        #sentences = nltk.tokenize.sent_tokenize(train_data)
+    def __init__(self, train_data, n):
+        sentences = nltk.tokenize.sent_tokenize(train_data)
         sentences = train_data.split('.')
 
-        self.vocab = self.build_vocab(list(map(preprocess, sentences)))
+        tokens = preprocess(sentences, n)
+        self.vocab = self.build_model(tokens, n)
+
+    def build_model(self, tokens, n):
+        ngrams = nltk.ngrams(tokens, n)
+        nvocab = nltk.FreqDist(ngrams)
+
+        if n == 1:
+            vocab = nltk.FreqDist(tokens)
+            vocab_size = len(nvocab)
+            return {v: c/vocab_size for v, c in vocab.items()}
+        else:
+            mgrams = nltk.ngrams(tokens, n-1)
+            mvocab = nltk.FreqDist(mgrams)
+            def ngram_prob(ngram, ncount):
+                mgram = ngram[:-1]
+                mcount = mvocab[mgram]
+                return ncount / mcount
+            return {v: ngram_prob(v, c) for v, c in nvocab.items()}
 
     def smoothing(smt_type='laplace'):
         if smt_type == 'laplace':
@@ -35,4 +82,9 @@ class SimpleNgramLanguageModel():
         return vocab
 
 
-SimpleNgramLanguageModel(text)
+lm = SimpleNgramLanguageModel(text, n=3)
+vocab = lm.vocab
+for k, v in vocab.items():
+    given = k[:-1]
+    word = k[-1]
+    print(f'P({word}|{given}): {v}')
